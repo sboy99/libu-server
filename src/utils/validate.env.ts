092a7bc6af3913c5a170d5cb6b3069e455f47333
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-05 01:40:38 +0530

    feat: added utili functions

---
+++ src/utils/validate.env.ts
@@ -0,0 +1,15 @@
+import { cleanEnv, port, str } from 'envalid';
+
+const validateEnv = (): void => {
+  cleanEnv(process.env, {
+    NODE_ENV: str({
+      choices: ['development', 'production'],
+    }),
+    MONGO_DB_USER: str(),
+    MONGO_DB_PWD: str(),
+    MONGO_DB_PATH: str(),
+    PORT: port({ default: 5000 }),
+  });
+};
+
+export default validateEnv;
