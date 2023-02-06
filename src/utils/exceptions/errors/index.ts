f89b75bb2ccdfb610dd1f8a3a0be3d04ccb29479
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-07 00:39:02 +0530

    feat: added unauthorized custom error

--- src/utils/exceptions/errors/index.ts
+++ src/utils/exceptions/errors/index.ts
@@ -1,11 +1,13 @@
 import BadRequestError from '@/utils/exceptions/errors/badRequest.error';
 import ForbiddenError from '@/utils/exceptions/errors/forbidden.error';
 import NotFoundError from '@/utils/exceptions/errors/notFound.error';
+import UnauthorizedError from '@/utils/exceptions/errors/unauthorized.error';
 
 const Errors = {
   BadRequestError,
   ForbiddenError,
   NotFoundError,
+  UnauthorizedError,
 };
 
 export default Errors;
