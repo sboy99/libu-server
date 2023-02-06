1a1a6b5d08f752caba9ade7fec06a80cdae99981
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-07 00:36:07 +0530

    fix: resolved forbidden statatus code

--- src/utils/exceptions/errors/forbidden.error.ts
+++ src/utils/exceptions/errors/forbidden.error.ts
@@ -2,7 +2,7 @@ import HttpException from '@/utils/exceptions/http.exception';
 
 class ForbiddenError extends HttpException {
   constructor(message = `Don't have permission to access`) {
-    super(400, message);
+    super(403, message);
   }
 }
 
