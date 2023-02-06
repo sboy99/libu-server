525a1ed28a77e283e55ada7188eec861a75ac5a5
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-07 00:36:44 +0530

    fix: resolved not found status code

--- src/utils/exceptions/errors/notFound.error.ts
+++ src/utils/exceptions/errors/notFound.error.ts
@@ -2,7 +2,7 @@ import HttpException from '@/utils/exceptions/http.exception';
 
 class NotFoundError extends HttpException {
   constructor(message = 'Item not found') {
-    super(400, message);
+    super(404, message);
   }
 }
 
