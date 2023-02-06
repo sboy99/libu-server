c2f99e9dead32d4f76637e89b1c5f72dc3680dbf
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-07 00:43:21 +0530

    feat: added validation middleware that validates body and params of incoming payload

---
+++ src/middlewares/validation.middleware.ts
@@ -0,0 +1,17 @@
+import type ApiRequestHandler from '@/interfaces/apiRequestHandler.interface';
+import type { AnyZodObject } from 'zod';
+
+type Validator = (vSchema: AnyZodObject) => ApiRequestHandler;
+
+export const validateBody: Validator = (vSchema) => async (req, res, next) => {
+  const validData = await vSchema.parseAsync(req.body);
+  req.body = validData;
+  next();
+};
+
+export const validateParams: Validator =
+  (vSchema) => async (req, res, next) => {
+    const validData = await vSchema.parseAsync(req.params);
+    req.body = validData;
+    next();
+  };
