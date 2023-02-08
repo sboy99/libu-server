89919952ad7fabe06b2a82ee473b3cf6a410d473
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-08 12:23:46 +0530

    feat: added utilities

--- src/interfaces/app.interface.ts
+++ src/interfaces/app.interface.ts
@@ -1,5 +1,36 @@
 import type IRoute from '@/interfaces/route.interface';
+import type { Request, Response } from 'express';
+import type { Types } from 'mongoose';
+
+interface JwtPayload {
+  userId: string;
+  userName: string;
+  role: 'reader' | 'librarian' | 'owner';
+}
+
+type UserTokenPayload = {
+  ip: string;
+  refreshToken: string;
+  userAgent: string;
+  userId: Types.ObjectId;
+};
 
 export declare type IReturnsVoid = () => void;
-
 export declare type IInitializeRoutes = (routes: IRoute[]) => void;
+
+export declare type GetAccessToken = (payload: JwtPayload) => string;
+export declare type GetRefreshToken = (
+  payload: JwtPayload,
+  refreshToken: string
+) => string;
+
+export declare type AttachCookies = (
+  res: Response,
+  payload: JwtPayload,
+  refreshToken: string
+) => void;
+
+export declare type GetUserLoginToken = (
+  req: Request,
+  userId: Types.ObjectId
+) => Promise<UserTokenPayload | never>;
--- src/utils/global.ts
+++ src/utils/global.ts
@@ -1,2 +1,6 @@
 export const MONGO_ID = /^[a-f\d]{24}$/i;
 export const PW_REGX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+export const _15MINS = 1000 * 60 * 15;
+export const _1HR = 1000 * 60 * 60;
+export const _1DAY = 24 * _1HR;
+export const _3DAY = 3 * _1DAY;
