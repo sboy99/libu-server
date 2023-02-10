3fd57dc88090f597542e7047ce016397847b4231
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-10 18:03:07 +0530

    refactor: updated types according to validation

--- src/resources/token/token.interface.ts
+++ src/resources/token/token.interface.ts
@@ -1,5 +1,7 @@
+import type { Document, Types } from 'mongoose';
 import type { z } from 'zod';
 import TokenVSchema from './token.validation';
 
 const tokenVSchema = new TokenVSchema();
-export type TUserToken = z.infer<(typeof tokenVSchema)['create']>;
+export type TUserTokenDocument = z.infer<(typeof tokenVSchema)['create']> &
+  Document<Types.ObjectId>;
