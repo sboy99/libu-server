5d3c77ec82450fa3e7076104f8e5a5912e0b188f
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-10 18:07:06 +0530

    feat: added interface of custom validations

--- src/resources/auth/auth.interface.ts
+++ src/resources/auth/auth.interface.ts
@@ -5,3 +5,11 @@ const authVSchema = new AuthVSchema();
 
 export type TRegisterPayload = z.infer<(typeof authVSchema)['register']>;
 export type TLoginPayload = z.infer<(typeof authVSchema)['login']>;
+export type TVerifyOtpPayload = z.infer<(typeof authVSchema)['verifyOtp']>;
+export type TResendOtpPayload = z.infer<(typeof authVSchema)['resendOtp']>;
+export type TForgotPasswordPayload = z.infer<
+  (typeof authVSchema)['forgotPassword']
+>;
+export type TRestPasswordPayload = z.infer<
+  (typeof authVSchema)['resetPassword']
+>;
