4257910b5207794af95371266a6d5381138761c6
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-14 17:45:04 +0530

    added query validator middleware

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -4,14 +4,23 @@ import type { AnyZodObject } from 'zod';
 type Validator = (vSchema: AnyZodObject) => ApiRequestHandler;
 
 export const validateBody: Validator = (vSchema) => async (req, res, next) => {
-  const validData = await vSchema.parseAsync(req.body);
-  req.body = validData;
+  const validBody = await vSchema.parseAsync(req.body);
+  req.body = validBody;
   next();
 };
 
 export const validateParams: Validator =
   (vSchema) => async (req, res, next) => {
-    const validData = await vSchema.parseAsync(req.params);
-    req.params = validData;
+    const validParams = await vSchema.parseAsync(req.params);
+    for (const key in validParams) {
+      req.params[key] = validParams[key] as string;
+    }
+    next();
+  };
+
+export const validateQueries: Validator =
+  (vSchema) => async (req, res, next) => {
+    const validQuery = await vSchema.parseAsync(req.query);
+    req.query = validQuery;
     next();
   };
