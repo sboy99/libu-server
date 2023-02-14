d6f55f492b7b431c0baa000f2b8ee67e6065d6eb
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-14 17:59:07 +0530

    feat: enabled book & review routes

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ import App from '@/App';
 import validateEnv from '@/utils/validate.env';
 
 import AuthRoutes from '@/resources/auth/auth.routes';
+import BookRoutes from '@/resources/book-management/book/book.routes';
 import GenreRoutes from '@/resources/book-management/genre/genre.routes';
+import ReviewRoutes from '@/resources/book-management/review/review.routes';
 import UserRoutes from '@/resources/user/user.routes';
 
-const routes = [AuthRoutes, UserRoutes, GenreRoutes];
+const routes = [AuthRoutes, UserRoutes, BookRoutes, GenreRoutes, ReviewRoutes];
 
 validateEnv();
 const app = new App(
