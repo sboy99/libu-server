df959a29243ec9118e5fc502226460626319887a
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-14 17:52:22 +0530

    feat: added review interfaces

---
+++ src/resources/book-management/review/review.interface.ts
@@ -0,0 +1,20 @@
+import type { Document, Types } from 'mongoose';
+import type { z } from 'zod';
+import ReviewVSchema from './review.validation';
+
+export interface IAdditonalReviewFields {
+  reviewedBy: Types.ObjectId;
+  bookId: Types.ObjectId;
+  isDeleted: boolean;
+}
+
+const reviewVSchema = new ReviewVSchema();
+export type TCreateReview = z.infer<(typeof reviewVSchema)['create']>;
+export type TUpdateReview = z.infer<(typeof reviewVSchema)['update']>;
+export type TReviewQuery = z.infer<(typeof reviewVSchema)['reviewQuery']>;
+export type TReviewParams = z.infer<
+  (typeof reviewVSchema)['bookIdAndReviewId']
+>;
+export type TReviewDocument = TCreateReview &
+  IAdditonalReviewFields &
+  Document<Types.ObjectId>;
