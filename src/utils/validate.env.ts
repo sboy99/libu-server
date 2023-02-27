f0599730e1792c8eadb0c80997e17b6c6ff9940d
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-27 10:31:56 +0530

    feat: added more env validation rules

--- src/utils/validate.env.ts
+++ src/utils/validate.env.ts
@@ -1,16 +1,26 @@
-import { cleanEnv, port, str } from 'envalid';
+import { cleanEnv, num, port, str } from 'envalid';
 
 const validateEnv = (): void => {
   cleanEnv(process.env, {
     NODE_ENV: str({
       choices: ['development', 'production'],
     }),
+    PORT: port({ default: 5000 }),
     MONGO_DB_USER: str(),
     MONGO_DB_PWD: str(),
     MONGO_DB_PATH: str(),
     COOKIE_SECRET: str(),
     JWT_SECRET: str(),
-    PORT: port({ default: 5000 }),
+    SMTP_HOST: str(),
+    SMTP_PORT: port(),
+    SMTP_TLS: str(),
+    SMTP_USERNAME: str(),
+    SMTP_PASSWORD: str(),
+    SENDER_NAME: str(),
+    SENDER_EMAIL: str(),
+    CLOUDINARY_CLOUD_NAME: str(),
+    CLOUDINARY_API_KEY: num(),
+    CLOUDINARY_API_SECRET: str(),
   });
 };
 
