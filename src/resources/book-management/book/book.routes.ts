136ab8941f4b6be3a6b8cb3083bfe45b265531f1
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-28 12:38:35 +0530

    feat: enabled validation middlewares in routes

--- src/resources/book-management/book/book.routes.ts
+++ src/resources/book-management/book/book.routes.ts
@@ -2,6 +2,11 @@ import type IRoute from '@/interfaces/route.interface';
 import authenticate, {
   hasPermission,
 } from '@/middlewares/authentication.middleware';
+import imageUploader from '@/middlewares/imageUpload.middleware';
+import {
+  validateBody,
+  validateParams,
+} from '@/middlewares/validation.middleware';
 import { Router } from 'express';
 import BookController from './book.controller';
 import BookVSchema from './book.validation';
@@ -19,24 +24,40 @@ class BookRoutes implements IRoute {
   }
   private initializeRoutes = () => {
     /**
-     * endpoint `/api/v1/genres/`
+     * endpoint `/api/v1/books/`
      */
-    this.router.route('/').get(this.controller.readAll).post(
+    this.router
+      .route('/')
+      .get(this.controller.readAll)
+      .post(
+        authenticate,
+        hasPermission('librarian', 'owner'),
+        validateBody(this.vSchema.create),
+        this.controller.create
+      );
+    /**
+     * endpoing `/api/v1/books/upload`
+     */
+    this.router.route('/upload').post(
       authenticate,
       hasPermission('librarian', 'owner'),
-      // validateBody(this.vSchema.create),
-      this.controller.create
+      imageUploader('image', {
+        maxUploadSize: 2,
+        uploadType: 'single',
+      }),
+      this.controller.imageUpload
     );
     /**
-     * endpoint `/api/v1/genres/:id`
+     * endpoint `/api/v1/books/:id`
      */
     this.router
       .route('/:id')
-      .get(this.controller.readOneById)
+      .get(validateParams(this.vSchema.params), this.controller.readOneById)
       .patch(
+        validateParams(this.vSchema.params),
         authenticate,
         hasPermission('librarian', 'owner'),
-        // validateBody(this.vSchema.create),
+        validateBody(this.vSchema.update),
         this.controller.updateOneById
       )
       .delete(
