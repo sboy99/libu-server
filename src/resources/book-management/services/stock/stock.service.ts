0c4c8a53eca6f613a4a59cd915fef6763bf75ca4
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-28 21:45:14 +0530

    feat: book resource now supports stock service

--- src/resources/book-management/services/stock/stock.service.ts
+++ src/resources/book-management/services/stock/stock.service.ts
@@ -1,20 +1,45 @@
+import type { Types } from 'mongoose';
 import StockModel from './stock.model';
 import StockVSchema from './stock.validation';
 
 class StockService {
   private db = StockModel;
   private vSchema = new StockVSchema();
+  /**
+   *
+   * @param data unknown
+   * @returns validated zod data
+   */
   private validateStockData = async (data: unknown) => {
     return await this.vSchema.upsertStock.parseAsync(data);
   };
+  /**
+   *
+   * @param bookId ObjectId
+   * @returns initial stock
+   */
+  public resetStock = async (bookId: Types.ObjectId) => {
+    const resetData = await this.validateStockData({});
+    return await this.db
+      .findOneAndUpdate({ bookId }, resetData, { new: true, lean: true })
+      .select('-createdAt -updatedAt -isDeleted -__v');
+  };
+  /**
+   *
+   * @param bookId ObjectId
+   * @param data unknown
+   * @returns stock
+   */
+  public upsertStock = async (bookId: Types.ObjectId, data: unknown) => {
+    const validStockData = await this.validateStockData(data);
+    const stock = await this.db
+      .findOneAndUpdate({ bookId, isDeleted: false }, validStockData, {
+        upsert: true,
+        new: true,
+        lean: true,
+      })
+      .select('-createdAt -updatedAt -isDeleted -__v');
 
-  public upsertStock = async (data: unknown) => {
-    const { bookId, ...stockData } = await this.validateStockData(data);
-    const stock = await this.db.updateOne({ bookId }, stockData, {
-      new: true,
-      upsert: true,
-      lean: true,
-    });
     return stock;
   };
 }
