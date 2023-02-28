656b033db941a96f78c1a37c7cffdf4402285dfe
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-28 11:46:43 +0530

    perf: enabled error handling

--- src/utils/getObjectIdFromString.ts
+++ src/utils/getObjectIdFromString.ts
@@ -3,8 +3,14 @@ import type { Types } from 'mongoose';
 import { isValidObjectId } from 'mongoose';
 import Errors from './exceptions/errors';
 
-export default function getObjectIdFromString(str: string): Types.ObjectId {
-  if (!isValidObjectId(str))
+export default function getObjectIdFromString(
+  str: string
+): Types.ObjectId | never {
+  try {
+    if (!isValidObjectId(str))
+      throw new Errors.BadRequestError('Object id is not valid');
+    return new ObjectId(str) as Types.ObjectId;
+  } catch (error) {
     throw new Errors.BadRequestError('Object id is not valid');
-  return new ObjectId(str) as Types.ObjectId;
+  }
 }
