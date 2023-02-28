4a95649084c025ad8b18ac3f8412f235e5aa5835
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-28 12:33:50 +0530

    feat: updated review model

--- src/resources/book-management/review/review.model.ts
+++ src/resources/book-management/review/review.model.ts
@@ -1,5 +1,16 @@
-import { Schema, model } from 'mongoose';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import type { Types } from 'mongoose';
 import type { TReviewDocument } from './review.interface';
+
+import Errors from '@/utils/exceptions/errors';
+import { Schema, model } from 'mongoose';
+
+interface IAggregateReviews {
+  _id: Types.ObjectId;
+  avgRatings: number;
+  totalReviews: number;
+}
 
 const reviewSchema = new Schema<TReviewDocument>(
   {
@@ -50,7 +61,41 @@ const reviewSchema = new Schema<TReviewDocument>(
     virtuals: true,
   }
 );
+reviewSchema.index({ bookId: 1, reviewedBy: 1 }, { unique: true });
 
+reviewSchema.statics.calculateReviews = async function (
+  bookId: Types.ObjectId
+) {
+  const aggregationResult = await this.aggregate([
+    {
+      $match: { bookId: bookId, isDeleted: false },
+    },
+    {
+      $group: {
+        _id: bookId,
+        avgRatings: { $avg: '$ratings' },
+        totalReviews: { $sum: 1 },
+      },
+    },
+  ]);
+
+  try {
+    if (aggregationResult.length) {
+      const result = aggregationResult[0] as IAggregateReviews;
+      await model('Book').findByIdAndUpdate(bookId, {
+        ratings: Number(result.avgRatings.toFixed(1)),
+        totalReviews: result.totalReviews,
+      });
+    }
+  } catch (error) {
+    throw new Errors.NotFoundError('book not found');
+  }
+};
+
+reviewSchema.post('save', async function () {
+  // @ts-ignore
+  await this.constructor.calculateReviews(this.bookId);
+});
 // reviewSchema.virtual('review').get(function () {
 //   if (this.reviews && this.reviews?.length > 0) {
 //     return this.reviews[this.reviews.length - 1];
